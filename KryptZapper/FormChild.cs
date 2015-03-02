87dbb97dbdd44b1a3dba245a1559c86fa093d7a5
Nadia Dobrianskaia <nadezhdadobrianskaia@gmail.com>
2015-03-02 09:06:00 -0800

    made a parent form, a child form, added a menue strip and about dialog box set up

--- KryptZapper/FormChild.cs
+++ KryptZapper/FormChild.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace KryptZapper
 {
-    public partial class Form1 : Form
+    public partial class FormChild : Form
     {
-        public Form1()
+        public FormChild()
         {
             InitializeComponent();
         }
