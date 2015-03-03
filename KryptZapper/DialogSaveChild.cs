4dc39b1f7ca300870382ca90f8a9f90d9538eb71
Becky <beckyzhou@outlook.com>
2015-03-03 09:08:59 -0800

    Styling changes

--- KryptZapper/DialogSaveChild.cs
+++ KryptZapper/DialogSaveChild.cs
@@ -16,5 +16,15 @@ namespace KryptZapper
         {
             InitializeComponent();
         }
+
+        private void textBoxSave_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DialogSaveChildLabel_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
