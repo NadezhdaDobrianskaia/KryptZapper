50832090d131ab40d89a100d7ffed72aa51b67a7
rosannawubs@gmail.com <rosannawubs@gmail.com>
2015-03-02 19:40:03 -0800

    changes

--- KryptZapper/FormChild.cs
+++ KryptZapper/FormChild.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,6 +16,16 @@ namespace KryptZapper
     {
         string ext = null; //added for the saveAs
         string justSave = null; //added for the saveAs
+
+        //For Encryption
+        string passPhrase = "Pasword";        // can be any string
+        string saltValue = "sALtValue";        // can be any string
+        string hashAlgorithm = "SHA1";             // can be "MD5"
+        int passwordIterations = 7;                  // can be any number
+        string initVector = "~1B2c3D4e5F6g7H8"; // must be 16 bytes
+        int keySize = 256;                // can be 192 or 128
+        //---end for Encryption
+
 
         public FormChild()
         {
@@ -93,6 +105,38 @@ namespace KryptZapper
             {
                 this.Dispose();
             }
+        }
+
+
+
+
+        String MyEncryptedText;
+        public void EncryptChild()
+        {
+            string text = richTextBox1.Text;
+            MyEncryptedText = Encrypt(text);
+            richTextBox1.Text = MyEncryptedText;
+        }
+
+        private string Encrypt(string data)
+        {
+            MessageBox.Show("Trying to Encrypt");
+            
+            byte[] bytes = Encoding.ASCII.GetBytes(this.initVector);
+            byte[] rgbSalt = Encoding.ASCII.GetBytes(this.saltValue);
+            byte[] buffer = Encoding.UTF8.GetBytes(data);
+            byte[] rgbKey = new PasswordDeriveBytes(this.passPhrase, rgbSalt, this.hashAlgorithm, this.passwordIterations).GetBytes(this.keySize / 8);
+            RijndaelManaged managed = new RijndaelManaged();
+            managed.Mode = CipherMode.CBC;
+            ICryptoTransform transform = managed.CreateEncryptor(rgbKey, bytes);
+            MemoryStream stream = new MemoryStream();
+            CryptoStream stream2 = new CryptoStream(stream, transform, CryptoStreamMode.Write);
+            stream2.Write(buffer, 0, buffer.Length);
+            stream2.FlushFinalBlock();
+            byte[] inArray = stream.ToArray();
+            stream.Close();
+            stream2.Close();
+            return Convert.ToBase64String(inArray);
         }
     }
 }
